3adb0fd2f160101648a92e0cfb5fa8739b6a1148
walon <walon@code-philosophy.com>
2026-01-22 12:31:33 +0800

    new: add file for log.

---
+++ src/runtime/log/internal_logger.h
@@ -0,0 +1,30 @@
+#pragma once
+
+namespace leanclr::log
+{
+
+#define LOG_LEVEL_DEBUG 0
+#define LOG_LEVEL_INFO 1
+#define LOG_LEVEL_WARNING 2
+#define LOG_LEVEL_ERROR 3
+#define LOG_LEVEL_FATAL 4
+#define LOG_LEVEL_NONE 5
+
+#ifndef LEANCLR_LOG_LEVEL
+#ifdef NDEBUG
+#define LEANCLR_LOG_LEVEL LOG_LEVEL_INFO
+#else
+#define LEANCLR_LOG_LEVEL LOG_LEVEL_DEBUG
+#endif
+#endif
+
+class InternalLogger
+{
+  public:
+    static void debug(const char* message);
+    static void info(const char* message);
+    static void warning(const char* message);
+    static void error(const char* message);
+    static void fatal(const char* message);
+};
+} // namespace leanclr::log
